f3e5dcfa016f883144ef1d4a34a1d1bfa5a85e5d
fer <ferconde87@gmail.com>
2021-12-25 15:00:57 +0700

    227. Basic Calculator II

--- 143. Reorder List/reoder_list.rb
+++ 143. Reorder List/reoder_list.rb
@@ -39,4 +39,33 @@ def reorder_list(head)
       left.next = nil
   end
   head
+end 
+
+
+# Runtime: 76 ms, faster than 86.96% of Ruby online submissions for Reorder List.
+# Memory Usage: 212.7 MB, less than 91.30% of Ruby online submissions for Reorder List.
+def reorder_list(head)
+  stack = []
+  node = head
+  n = 0
+  while node != nil
+      stack << node
+      node = node.next
+      n += 1
+  end
+  left = head
+  half = n/2
+  (half).times do
+      right = stack.pop
+      break if left == right
+      next_left = left.next
+      left.next = right
+      if (right == next_left)
+          right.next = nil
+          break
+      end
+      right.next = next_left
+      left = next_left
+  end
+  left.next = nil if (n % 2 == 1)
 end
